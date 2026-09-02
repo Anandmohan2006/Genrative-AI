f7489072ac0f3c698ee57c6dbbbb9f96052f608a
AnandMohan2006 <dubeysatyam95298@gmail.com>
2026-09-02 11:16:49 +0530

    Added new topic

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,18 +1,26 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 
-from langchain.chat_models import init_chat_model
+# from langchain.chat_models import init_chat_model
 
-model = init_chat_model(
-    "mistral-small-latest",
-    model_provider="mistralai", temperature=1, max_output_tokens=55
+# model = init_chat_model(
+#     "mistral-small-latest",
+#     model_provider="mistralai", temperature=1, max_output_tokens=55
+# )
+
+
+
+# # print(model.invoke("what is love?"))
+# answer = model.invoke("why people fall in love? ")
+
+# print(answer)
+
+
+from langchain_mistralai import ChatMistralAI
+
+model = ChatMistralAI(
+    model="mistral-small-latest",
+    api_key="YOUR_API_KEY"
 )
-
-
-
-# print(model.invoke("what is love?"))
-answer = model.invoke("why people fall in love? ")
-
-print(answer)
---
+++ chatmodels/chatbot.py
@@ -0,0 +1,23 @@
+from langchain_mistralai import ChatMistralAI
+
+model = ChatMistralAI(
+    model="mistral-small-latest",
+    api_key="YOUR_API_KEY"
+)
+
+
+
+
+
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model(
+    "mistral-small-latest",
+    model_provider="mistralai", temperature=1
+)
+
+prompt = input("you: ")
+response = model.invoke(prompt)
+print("bot:", response)
+
